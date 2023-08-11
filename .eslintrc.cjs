a357248cf75f843477b6b8957a2848c037200b87
Alex Chavez <alex@alexchavez.net>
2023-08-11 11:48:20 -0700

    Configure typescript-eslint for the whole project

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.eslint.json',
+      './api/tsconfig.json',
+      './web/tsconfig.json'
+    ],
+  },
+  root: true,
+  ignorePatterns: ['.eslintrc.cjs'],
+};
